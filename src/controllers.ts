e8ae51a9d5706219e40681a6a9875c68bddc8c39
Dan O'Connor <dan@strata.ltd>
2023-08-26 14:48:33 -0400

    refactor: added basic error handling to randomImg controller

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,8 +1,15 @@
-import { RequestHandler } from "express";
+import type { RequestHandler } from "express";
+
 import { createApi } from "unsplash-js";
 import nodeFetch from "node-fetch";
 
 const accessKey = process.env.ACCESS_KEY;
+
+const dummyResult = {
+  result: {
+    response: [],
+  },
+};
 
 const unsplash = createApi({
   accessKey: accessKey as string,
@@ -10,12 +17,27 @@ const unsplash = createApi({
   //...other fetch options
 });
 
-export const randomImg: RequestHandler = async (_, res) => {
-  unsplash.photos.getRandom({}).then((result) => {
-    if (result.errors) {
-      console.log("error occurred: ", result.errors[0]);
-    } else {
-      res.send(result);
-    }
-  });
+export const randomImg: RequestHandler = (req, res) => {
+  const { count } = req.query;
+
+  unsplash.photos
+    .getRandom({
+      count: Number(count),
+    })
+    .then((result) => {
+      if (result.type === "error") {
+        // TODO: handle error – sentry?
+        console.error("error occurred: ", result.errors[0]);
+
+        res.send(dummyResult);
+      } else {
+        res.send(result);
+      }
+    })
+    .catch((error) => {
+      // TODO: handle error – sentry?
+      console.error(error);
+
+      res.send(dummyResult);
+    });
 };
